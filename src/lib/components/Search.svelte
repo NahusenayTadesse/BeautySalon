d7f11c522af7abe726d2a6ead41eac2a55446fa5
NahusenayTadesse <124673344+NahusenayTadesse@users.noreply.github.com>
2026-01-13 00:16:41 +0300

    Colors changed

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -6,30 +6,63 @@
 	let isOpen = $state(false);
 	let list = [
 		{ label: 'Dashboard', path: '/dashboard' },
+		{ label: 'Change Password', path: '/dashboard/change-password' },
 
+		{ label: 'Admin Panel', path: '/dashboard/admin-panel' },
 		{ label: 'Add Positions', path: '/dashboard/admin-panel/add-positions' },
 		{ label: 'Add Product Category', path: '/dashboard/admin-panel/add-product-category' },
 		{ label: 'Add Roles', path: '/dashboard/admin-panel/add-roles' },
-		{ label: 'Add Services (Admin Panel)', path: '/dashboard/admin-panel/add-services' },
-		{ label: 'Add Users', path: '/dashboard/admin-panel/add-users' },
+		{ label: 'Add Services (Admin)', path: '/dashboard/admin-panel/add-services' },
+		{ label: 'Add Users (Admin)', path: '/dashboard/admin-panel/add-users' },
+		{ label: 'Payment Methods', path: '/dashboard/admin-panel/payment-methods' },
+		{ label: 'Roles', path: '/dashboard/admin-panel/roles' },
+		{ label: 'Add Role', path: '/dashboard/admin-panel/roles/add-roles' },
+		{ label: 'Role Details', path: '/dashboard/admin-panel/roles/:id' },
 
+		{ label: 'Appointments', path: '/dashboard/appointments' },
 		{ label: 'Add Appointment', path: '/dashboard/appointments/add-appointment' },
 		{ label: 'All Appointments', path: '/dashboard/appointments/all-appointments' },
+		{ label: 'Appointments by Range', path: '/dashboard/appointments/all-appointments/:range' },
+		{ label: 'Appointment Details', path: '/dashboard/appointments/single/:id' },
+		{ label: 'Appointments by Date', path: '/dashboard/appointments/:date' },
 
-		{ label: 'Add Inventory', path: '/dashboard/inventory/add-inventory' },
+		{ label: 'Customers', path: '/dashboard/customers' },
 
-		{ label: 'Add Products', path: '/dashboard/products/add-products' },
+		{ label: 'Products', path: '/dashboard/products' },
+		{ label: 'Add Product', path: '/dashboard/products/add-products' },
+		{ label: 'Product Categories', path: '/dashboard/products/categories' },
 
 		{ label: 'Reports', path: '/dashboard/reports' },
+		{ label: 'Reports by Range', path: '/dashboard/reports/:range' },
+
+		{ label: 'Salary', path: '/dashboard/salary' },
+
 		{ label: 'Sales', path: '/dashboard/sales' },
 
-		{ label: 'Add Services', path: '/dashboard/services/add-services' },
+		{ label: 'Services', path: '/dashboard/services' },
+		{ label: 'Add Service', path: '/dashboard/services/add-services' },
+		{ label: 'Service Categories', path: '/dashboard/services/categories' },
 
+		{ label: 'Staff', path: '/dashboard/staff' },
 		{ label: 'Add Staff', path: '/dashboard/staff/add-staff' },
+		{ label: 'Positions', path: '/dashboard/staff/positions' },
+		{ label: 'Staff Ranges', path: '/dashboard/staff/ranges/:range' },
 
+		{ label: 'Supplies', path: '/dashboard/supplies' },
 		{ label: 'Add Supplies', path: '/dashboard/supplies/add-supplies' },
+		{ label: 'Supply Details', path: '/dashboard/supplies/:id' },
 
-		{ label: 'Login', path: '/login' }
+		{ label: 'Transactions', path: '/dashboard/transactions' },
+		{ label: 'Expenses', path: '/dashboard/transactions/expenses' },
+		{ label: 'Add Expense', path: '/dashboard/transactions/expenses/add-expense' },
+		{
+			label: 'Add Expense Category',
+			path: '/dashboard/transactions/expenses/add-expense-category'
+		},
+		{ label: 'Expense Details', path: '/dashboard/transactions/expenses/single/:id' },
+
+		{ label: 'Users', path: '/dashboard/users' },
+		{ label: 'Add User', path: '/dashboard/users/add-users' }
 	];
 </script>
 
--- src/lib/global.svelte.ts
+++ src/lib/global.svelte.ts
@@ -1,6 +1,6 @@
 export const bgGradient = `bg-gradient-to-b from-[#e0f2ff] via-[#f0f7ff] to-[#ffffff]
 
-    dark:bg-gradient-to-tr dark:from-[#181818] dark:via-[#121212] dark:to-[#262626]/20`;
+    dark:bg-gradient-to-r dark:from-primary/40 dark:via-[#121212] dark:to-[#262626]/20`;
 export const selectItem = `hover:bg-gray-100 hover:shadow-md hover:scale-101 duration-300 transition-all ease-in-out dark:hover:bg-gray-900`;
 export const toastmsg = `fixed right-4 bottom-20 lg:bottom-4 z-50
              flex items-center gap-3
